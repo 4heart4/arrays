93eaa95754321e89c0b3dfdad2ef310647852300
4heart4 <kostur-b@i.ua>
2019-12-02 11:39:12 +0200

    Reverse

---
+++ src/ReverseSortedArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ReverseSortedArray {
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 1, 7, 11};
+
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+}
