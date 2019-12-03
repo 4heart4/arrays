5a88413760082df76f975537d693520f333b51b3
4heart4 <kostur-b@i.ua>
2019-12-03 13:19:54 +0200

    HW2

--- src/Duplicates.java
+++ src/Duplicates.java
@@ -5,12 +5,12 @@ public class Duplicates {
         int[] array = {3, 2, 3, 1, 4, 2, 8, 3};
 
         for (int i = 0; i < array.length; i++) {
-            for (int z = i + 1; z < array.length; z++) {
-                if (array[i] == (array[z])) {
-                    array[z] = 0;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == (array[j])) {
+                    array[j] = 0;
                 }
             }
         }
-            System.out.println(Arrays.toString(array));
-        }
+        System.out.println(Arrays.toString(array));
     }
+}
--- src/ReverseSortedArray.java
+++ src/ReverseSortedArray.java
@@ -5,10 +5,8 @@ import java.util.Collections;
 public class ReverseSortedArray {
     public static void main(String[] args) {
         Integer[] numbers = {2, 3, 1, 7, 11};
-        Arrays.sort(numbers);
         Arrays.sort(numbers, Collections.reverseOrder());
         for (int i = 0; i < numbers.length; i++) {
-
             System.out.println(numbers[i] + " ");
         }
     }
